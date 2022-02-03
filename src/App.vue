54910364bef5605306d779b5c8996162126c7adf
Cuke7 <cuke.dev@mail.com>
2022-02-03 19:20:49 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
     <v-app>
         <v-main>
-            <a href="http://192.168.1.37:9000/#!/home" target="_blank">
+            <a href="http://192.168.1.37:9000/#!/1/docker/containers" target="_blank">
                 <v-card class="pa-12 ma-12">
                     <v-card-title>Portainer</v-card-title>
                 </v-card>
             </a>
-            <a href="http://192.168.1.37:19999#menu_sensors" target="_blank">
-                <v-card class="pa-12 ma-12">
-                    <v-card-title>Netdata</v-card-title>
-                </v-card>
-            </a>
+
             <a href="https://192.168.1.37:443" target="_blank">
                 <v-card class="pa-12 ma-12">
                     <v-card-title>Nextcloud</v-card-title>
-                </v-card>
-            </a>
-            <a href="http://192.168.1.37:8083" target="_blank">
-                <v-card class="pa-12 ma-12">
-                    <v-card-title>calibre web</v-card-title>
                 </v-card>
             </a>
         </v-main>
