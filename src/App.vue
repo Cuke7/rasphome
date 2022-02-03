9cc43144cd820af59e5e30118a0f931c69bb8cdd
Cuke7 <cuke.dev@mail.com>
2022-02-03 16:57:28 +0100

    tset

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,36 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <a href="http://192.168.1.37:9000/#!/home" target="_blank">
+                <v-card class="pa-12 ma-12">
+                    <v-card-title>Portainer</v-card-title>
+                </v-card>
+            </a>
+            <a href="http://192.168.1.37:19999" target="_blank">
+                <v-card class="pa-12 ma-12">
+                    <v-card-title>Netdata</v-card-title>
+                </v-card>
+            </a>
+            <a href="https://192.168.1.37:443" target="_blank">
+                <v-card class="pa-12 ma-12">
+                    <v-card-title>Nextcloud</v-card-title>
+                </v-card>
+            </a>
+            <a href="http://192.168.1.37:8083" target="_blank">
+                <v-card class="pa-12 ma-12">
+                    <v-card-title>calibre web</v-card-title>
+                </v-card>
+            </a>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
+    name: "App",
 
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        //
+    }),
 };
 </script>
