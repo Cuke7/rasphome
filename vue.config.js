f1f70c7bc37bb12ffb8c33f0b2440a5b5f11704e
Cuke7 <cuke.dev@mail.com>
2022-02-04 17:18:06 +0100

    test

--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,4 @@ module.exports = {
     publicPath: "/rasphome/",
 
     transpileDependencies: ["vuetify"],
-
-    pwa: {
-        name: "Home",
-        themeColor: "#4DBA87",
-
-        workboxPluginMode: "GenerateSW",
-        icons: [
-            {
-                src: "./img/icons/logo.png",
-                sizes: "512x512",
-                type: "image/png",
-            },
-        ],
-    },
 };
