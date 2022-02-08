ac4bd59adc94198b6cb860acd076fac071d5bf56
Cuke7 <cuke.dev@mail.com>
2022-02-08 22:16:45 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <v-app>
         <v-main>
             <div class="white--text text-center text-h2 my-12">Bienvenue Louis</div>
-            <v-row class="ma-12">
+            <v-row class="ma-12" justify="center">
                 <v-col cols="12" md="4" v-for="(app, index) in apps" :key="index">
                     <a :href="getAdress(app)">
                         <v-card color="rgba(255,255,255,0.1)" class="my-4 pa-2">
@@ -20,86 +20,98 @@
                     </a>
                 </v-col>
             </v-row>
-            <!-- <v-row>
-                <v-card class="mx-12 pa-12" color="rgb(255, 255, 255, 0)" flat>
-                    <line-chart :chart-data="datacollection" :height="100" :options="options"></line-chart>
-                </v-card>
-            </v-row> -->
+            <v-row justify="center" class="pa-0 ma-0">
+                <v-col cols="12" md="8">
+                    <v-card class="pa-12" color="rgb(255, 255, 255, 0)" flat width="100%">
+                        <line-chart :chart-data="datacollection" height="100" :options="options"></line-chart>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-main>
     </v-app>
 </template>
 
 <script>
-// import axios from "axios";
-// import LineChart from "LineChart";
+import axios from "axios";
+// npm install chart.js@2
+import LineChart from "./LineChart";
 
 export default {
     name: "App",
 
-    // components: {
-    //     LineChart,
-    // },
+    components: {
+        LineChart,
+    },
 
     data() {
         return {
-            piAdress: "192.168.1.37",
+            piAdress: "192.168.1.63",
             apps: [
                 { nom: "Portainer", img: "./portainer.svg", extra: ":9000/#!/1/docker/containers", https: false },
                 { nom: "Nextcloud", img: "./nextcloud.svg", extra: ":443", https: true },
-                { nom: "Netdata", img: "./netdata.png", extra: ":19999/#menu_sensors", https: false },
-                { nom: "HomeAssistant", img: "./homeassistant.png", extra: ":8123", https: false },
             ],
-            // options: {
-            //     legend: {
-            //         labels: {
-            //             fontColor: "white",
-            //             fontSize: 18,
-            //         },
-            //     },
-            //     scales: {
-            //         yAxes: [
-            //             {
-            //                 ticks: {
-            //                     // beginAtZero: true,
-            //                     fontColor: "white",
-            //                 },
-            //             },
-            //         ],
-            //         xAxes: [
-            //             {
-            //                 ticks: {
-            //                     fontColor: "white",
-            //                 },
-            //             },
-            //         ],
-            //     },
-            // },
+            temperature: "",
+            options: {
+                legend: {
+                    labels: {
+                        fontColor: "white",
+                        fontSize: 18,
+                    },
+                },
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                // beginAtZero: true,
+                                fontColor: "white",
+                            },
+                        },
+                    ],
+                    xAxes: [
+                        {
+                            ticks: {
+                                fontColor: "white",
+                            },
+                        },
+                    ],
+                },
+            },
         };
     },
 
-    // computed: {
-    //     datacollection() {
-    //         let labels = this.temperature.time;
-    //         let data = this.temperature.temperature;
-    //         return {
-    //             labels: labels,
-    //             datasets: [
-    //                 {
-    //                     label: "Température de la Raspberry",
-    //                     backgroundColor: "rgba(0, 0, 0, 0)",
-    //                     data: data,
-    //                     borderColor: "#ffffff",
-    //                 },
-    //             ],
-    //         };
-    //     },
-    // },
+    computed: {
+        datacollection() {
+            let lines = this.temperature.split("\n");
+            lines.shift();
+            lines.pop();
+            let data = [];
+            let labels = [];
+            for (const line of lines) {
+                labels.push(line.split(",")[1]);
+                data.push(line.split(",")[2]);
+            }
+
+            console.log(lines);
+
+            return {
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Température de la Raspberry",
+                        backgroundColor: "rgba(0, 0, 0, 0)",
+                        data: data,
+                        borderColor: "#ffffff",
+                    },
+                ],
+            };
+        },
+    },
 
     mounted() {
-        // axios
-        //     .get("https://" + this.piAdress + ":3000")
-        //     .then((response) => (this.temperature = response.data))
-        //     .catch((error) => console.log(error));
+        axios
+            .get("https://cuke.duckdns.org/temperature.txt")
+            .then((response) => (this.temperature = response.data))
+            .catch((error) => console.log(error));
     },
 
     methods: {
