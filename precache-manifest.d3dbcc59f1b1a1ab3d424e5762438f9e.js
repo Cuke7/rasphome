560c63c8ae1468df1fd72458865afeeb579fc5e7
Louis <cuke.dev@gmail.com>
2022-02-20 11:45:00 +0100

    gh-pages

---
+++ precache-manifest.d3dbcc59f1b1a1ab3d424e5762438f9e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce2d79b96077099fd20cb6428f21507",
+    "url": "/rasphome/calibre.png"
+  },
+  {
+    "revision": "8c193d7d66f1000ff313",
+    "url": "/rasphome/css/app.4564e103.css"
+  },
+  {
+    "revision": "3d05d8faa0d18146aec7",
+    "url": "/rasphome/css/chunk-vendors.bcb93c39.css"
+  },
+  {
+    "revision": "6ae38958dc13affb4fe83ffdec7f62da",
+    "url": "/rasphome/homeassistant.png"
+  },
+  {
+    "revision": "2c5f729e34722f054a1f86647cc86b46",
+    "url": "/rasphome/index.html"
+  },
+  {
+    "revision": "d599e768a7cbf4b37cb5076703f3e3b4",
+    "url": "/rasphome/jellyfin.png"
+  },
+  {
+    "revision": "8c193d7d66f1000ff313",
+    "url": "/rasphome/js/app.90294adb.js"
+  },
+  {
+    "revision": "3d05d8faa0d18146aec7",
+    "url": "/rasphome/js/chunk-vendors.04a632bd.js"
+  },
+  {
+    "revision": "d379c8c87c4e5167849b2e48e588531f",
+    "url": "/rasphome/manifest.json"
+  },
+  {
+    "revision": "237aaab1decb50ba94117e0473865c83",
+    "url": "/rasphome/netdata.png"
+  },
+  {
+    "revision": "ad22713b75f3514f4aed76b3d5b83417",
+    "url": "/rasphome/nextcloud.svg"
+  },
+  {
+    "revision": "e7b616ce93bae55aa10a2323d84bbffb",
+    "url": "/rasphome/portainer.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rasphome/robots.txt"
+  }
+]);
